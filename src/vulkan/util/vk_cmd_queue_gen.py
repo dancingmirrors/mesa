bminor/mesa-mesa

commit 9082715ab014270fb8bdf555526d7e55a4890538

vk/cmd_queue: generate copies for string struct members
this includes e.g., debug labels, which previously was garbage data

Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34534>

src/vulkan/util/vk_cmd_queue_gen.py
@@ -529,19 +529,21 @@ def get_struct_copy(dst, src_name, src_type, size, types, level=0):
     tmp_dst = "%s *%s = (void *) %s; (void) %s;" % (src_type, tmp_dst_name, dst, tmp_dst_name)
     tmp_src = "%s *%s = (void *) %s; (void) %s;" % (src_type, tmp_src_name, src_name, tmp_src_name)
     member_copies = ""
+    indent = "\n%s" % ("   " * (level + 1))
+    indent_sameline = "\n%s" % ("   " * level)
     if src_type in types:
         for member in types[src_type].members:
             if member.len and member.len == 'struct-ptr':
                 member_copies += get_struct_copy("%s->%s" % (tmp_dst_name, member.name), "%s->%s" % (tmp_src_name, member.name), member.type, 'sizeof(%s)' % member.type, types, level + 1)
-            elif member.len and member.len != 'null-terminated':
+            elif member.len and member.len == 'null-terminated':
+                member_copies += "%s%s->%s = strdup(%s->%s);" % (indent_sameline, tmp_dst_name, member.name, tmp_src_name, member.name)
+            elif member.len:
                 member_copies += get_array_member_copy(tmp_dst_name, tmp_src_name, member, level + 1)
             elif member.name == 'pNext':
                 member_copies += get_pnext_member_copy(tmp_dst_name, src_type, member, types, level + 1)
 
     null_assignment = "%s = NULL;" % dst
     if_stmt = "if (%s) {" % src_name
-    indent = "\n%s" % ("   " * (level + 1))
-    indent_sameline = "\n%s" % ("   " * level)
     return "%s%s%s%s%s%s%s%s%s%s%s%s} else {%s%s%s}" % (if_stmt, indent, allocation, indent, copy, indent, tmp_dst, indent, tmp_src, indent, member_copies, indent_sameline, indent, null_assignment, indent_sameline)
 
 def get_struct_free(command, param, types):

dancingmirrors/mesa

commit 7e6548edf24c61a89e8e05cb2dd7981bafdf18e0

vulkan/cmd_queue: Use vk_strdup and free allocated string memory
Fixes: 9082715ab01 ("vk/cmd_queue: generate copies for string struct members")

Signed-off-by: Aitor Camacho <aitor@lunarg.com>

src/vulkan/util/vk_cmd_queue_gen.py
@@ -578,7 +578,7 @@ def get_struct_copy(builder, dst, src_name, src_type, types, parent_name=None, l
                     tmp_src_name, member.name
                 ), member.type, types, tmp_src_name, member.len)
             elif member.len and member.len == 'null-terminated':
-                builder.add("%s->%s = strdup(%s->%s);" % (tmp_dst_name, member.name, tmp_src_name, member.name))
+                builder.add("%s->%s = vk_strdup(queue->alloc, %s->%s, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);" % (tmp_dst_name, member.name, tmp_src_name, member.name))
             elif member.name == 'pNext':
                 get_pnext_member_copy(builder, tmp_dst_name, src_type, member, types)
 
@@ -637,6 +637,8 @@ def get_struct_free(builder, field_name, nullable, struct_type, types, parent_na
                 builder.add("}")
 
             builder.add("vk_free(queue->alloc, %s);" % (local_member_name))
+        elif member.len and member.len == 'null-terminated':
+            builder.add("vk_free(queue->alloc, (void*)%s);" % (member_name))
 
         elif member.name == 'pNext':
             get_pnext_member_free(builder, struct_type, types, member_name)