62d3ed2bd403aec610d029afd7cc47eadf20e4b6
dancingmirrors <dancingmirrors@icloud.com>
2025-12-01 22:51:54 -0700

    Remove pointless LIBGL_DRIVERS_PATH from the test script.

--- hasvk-test.sh
+++ hasvk-test.sh
@@ -4,7 +4,6 @@ MESA_BUILD_DIR=$PWD
 MESA_LIB_DIRS=$(find "$MESA_BUILD_DIR" -name '*.so*' -exec dirname {} \; | sort -u | paste -sd: -)
 
 export LD_LIBRARY_PATH=$MESA_LIB_DIRS:$LD_LIBRARY_PATH
-export LIBGL_DRIVERS_PATH=$MESA_BUILD_DIR/lib64/dri:$MESA_BUILD_DIR/lib/dri
 export VK_ICD_FILENAMES=$MESA_BUILD_DIR/build/src/intel/vulkan_hasvk/intel_hasvk_devenv_icd.x86_64.json
 export VK_ICD_FILENAMES=$VK_ICD_FILENAMES:$MESA_BUILD_DIR/build-32/src/intel/vulkan_hasvk/intel_hasvk_devenv_icd.i686.json
 
