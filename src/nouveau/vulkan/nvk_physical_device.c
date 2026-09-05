alexvorxx/zink-xlib-termux

commit a9214460eec38500919cd0eec5513dce987ffb32

nvk: Expose a host-visible VRAM type when we have REBAR
We use host maps of VRAM all over the driver on Maxwell+ so we know they
work.  This exposes it to applications for use for data upload.  To
avoid thrashing, we only expose this on systems with properly a
configured PCI controllwer with resizable BAR support.

We already choose whether or not to set the MMAP bit when creating the
BO based on memory type properties so we just need to add the type.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26622>

src/nouveau/vulkan/nvk_physical_device.c
@@ -1127,6 +1127,16 @@ nvk_create_drm_physical_device(struct vk_instance *_instance,
          .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
          .heapIndex = vram_heap_idx,
       };
+
+      if (pdev->info.cls_eng3d >= MAXWELL_A &&
+          pdev->info.bar_size_B >= pdev->info.vram_size_B) {
+         pdev->mem_types[pdev->mem_type_count++] = (VkMemoryType) {
+            .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
+                             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                             VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            .heapIndex = vram_heap_idx,
+         };
+      }
    }
 
    uint32_t sysmem_heap_idx = pdev->mem_heap_count++;

src/nouveau/vulkan/nvk_physical_device.h
@@ -50,7 +50,7 @@ struct nvk_physical_device {
 
    // TODO: add mapable VRAM heap if possible
    struct nvk_memory_heap mem_heaps[2];
-   VkMemoryType mem_types[2];
+   VkMemoryType mem_types[3];
    uint8_t mem_heap_count;
    uint8_t mem_type_count;
 

dancingmirrors/mesa

commit 3c86e2cfd25b8c727125bd539c11fb8b9e6b2831

nvk: Expose BAR as host cached
nouveau will always uses CPU cached mappings unless we are on Tegra
and need coherency. This patch advertises host cached on BAR memory
type.

This is needed for gamescope as it explicitly search for a memory type
that is host visible and cached. Before this patch this would cause it
to always select sysmem for any allocations that need to be mappable.

This should hopefully at least help game recording and remoteplay
performance a bit.

Signed-off-by: Mary Guillemard <mary@mary.zone>
Fixes: a9214460eec3 ("nvk: Expose a host-visible VRAM type when we have REBAR")

src/nouveau/vulkan/nvk_physical_device.c
@@ -1652,7 +1652,8 @@ nvk_create_drm_physical_device(struct vk_instance *_instance,
          pdev->mem_types[pdev->mem_type_count++] = (VkMemoryType) {
             .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
                              VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                             VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                             VK_MEMORY_PROPERTY_HOST_COHERENT_BIT |
+                             VK_MEMORY_PROPERTY_HOST_CACHED_BIT,
             .heapIndex = bar_heap_idx,
          };
       }