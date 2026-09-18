aerisarn/mesa-uwp

commit c11ac5e4462c5aceb35958858f6cef93d142c7de

nir: Handle wider unaligned loads in lower_mem_access_bit_size
Reviewed-by: M Henning <drawoc@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21524>

src/compiler/nir/nir_lower_mem_access_bit_sizes.c
@@ -110,6 +110,8 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
       const unsigned bytes_left = bytes_read - chunk_start;
       const uint32_t chunk_align_offset =
          (whole_align_offset + chunk_start) % align_mul;
+      const uint32_t chunk_align =
+         nir_combined_align(align_mul, chunk_align_offset);
       requested = mem_access_size_align_cb(intrin->intrinsic, bytes_left,
                                            align_mul, chunk_align_offset,
                                            offset_is_const, cb_data);
@@ -118,9 +120,9 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
       assert(util_is_power_of_two_nonzero(requested.align));
       if (align_mul < requested.align) {
          /* For this case, we need to be able to shift the value so we assume
-          * there's at most one component.
+          * the alignment is less than the size of a single component.  This
+          * ensures that we don't need to upcast in order to shift.
           */
-         assert(requested.num_components == 1);
          assert(requested.bit_size >= requested.align * 8);
 
          uint64_t align_mask = requested.align - 1;
@@ -133,12 +135,43 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
                               requested.align, 0, NULL,
                               requested.num_components, requested.bit_size);
 
-         nir_ssa_def *shifted =
-            nir_ushr(b, &load->dest.ssa, nir_imul_imm(b, pad, 8));
+         unsigned max_pad = requested.align - chunk_align;
+         unsigned requested_bytes =
+            requested.num_components * requested.bit_size / 8;
+         chunk_bytes = MIN2(bytes_left, requested_bytes - max_pad);
 
-         chunk_bytes = MIN2(bytes_left, align_mul);
-         assert(num_chunks < ARRAY_SIZE(chunks));
-         chunks[num_chunks++] = nir_u2uN(b, shifted, chunk_bytes * 8);
+         nir_ssa_def *shift = nir_imul_imm(b, pad, 8);
+         nir_ssa_def *shifted = nir_ushr(b, &load->dest.ssa, shift);
+
+         if (load->dest.ssa.num_components > 1) {
+            nir_ssa_def *rev_shift =
+               nir_isub_imm(b, load->dest.ssa.bit_size, shift);
+            nir_ssa_def *rev_shifted = nir_ishl(b, &load->dest.ssa, rev_shift);
+
+            nir_ssa_def *comps[NIR_MAX_VEC_COMPONENTS];
+            for (unsigned i = 1; i < load->dest.ssa.num_components; i++)
+               comps[i - 1] = nir_channel(b, rev_shifted, i);
+
+            comps[load->dest.ssa.num_components - 1] =
+               nir_imm_zero(b, 1, load->dest.ssa.bit_size);
+
+            rev_shifted = nir_vec(b, comps, load->dest.ssa.num_components);
+            shifted = nir_bcsel(b, nir_ieq_imm(b, shift, 0), &load->dest.ssa,
+                                   nir_ior(b, shifted, rev_shifted));
+         }
+
+         unsigned chunk_bit_size = MIN2(8 << (ffs(chunk_bytes) - 1), bit_size);
+         unsigned chunk_num_components = chunk_bytes / (chunk_bit_size / 8);
+
+         /* There's no guarantee that chunk_num_components is a valid NIR
+          * vector size, so just loop one chunk component at a time
+          */
+         for (unsigned i = 0; i < chunk_num_components; i++) {
+            assert(num_chunks < ARRAY_SIZE(chunks));
+            chunks[num_chunks++] =
+               nir_extract_bits(b, &shifted, 1, i * chunk_bit_size,
+                                1, chunk_bit_size);
+         }
       } else if (chunk_align_offset % requested.align) {
          /* In this case, we know how much to adjust the offset */
          uint32_t delta = chunk_align_offset % requested.align;

dancingmirrors/mesa

commit 58f9cee272eb9e965bcde3e601235faaa1f2427c

nir: Handle unaligned base in lower_mem_access_bit_sizes
Previously, the pass would handle base correctly as long as base was
aligned by the new alignment. The problem is, there's no reason base
would be aligned that way when we're widening alignments. Handle that
case by applying base to offset wherever we manually align with iand.

Fixes misaligned address exceptions on nvk in
dEQP-VK.pipeline.pipeline_library.graphics_library.independent_sets_random.fast_lib.vert_geom_frag.case_7

Fixes: c11ac5e4462 ("nir: Handle wider unaligned loads in lower_mem_access_bit_size")
Fixes: c70d94a8897 ("nir_lower_mem_access_bit_sizes: Support unaligned stores via a pair of atomics")

src/compiler/nir/nir_lower_mem_access_bit_sizes.c
@@ -160,6 +160,8 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
    const uint32_t align_mul = nir_intrinsic_align_mul(intrin);
    const uint32_t whole_align_offset = nir_intrinsic_align_offset(intrin);
    const uint32_t whole_align = nir_intrinsic_align(intrin);
+   const uint32_t base =
+      nir_intrinsic_has_base(intrin) ? nir_intrinsic_base(intrin) : 0;
    const enum gl_access_qualifier access =
       nir_intrinsic_has_access(intrin) ? nir_intrinsic_access(intrin) : 0;
    nir_src *offset_src = nir_get_io_offset_src(intrin);
@@ -208,7 +210,7 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(requested.bit_size >= requested.align * 8);
 
          uint64_t align_mask = requested.align - 1;
-         nir_def *chunk_offset = nir_iadd_imm(b, offset, chunk_start);
+         nir_def *chunk_offset = nir_iadd_imm(b, offset, chunk_start + base);
 
          /* TODO add support for offset_shift. */
          assert(!nir_intrinsic_has_offset_shift(intrin) ||
@@ -222,6 +224,9 @@ lower_mem_load(nir_builder *b, nir_intrinsic_instr *intrin,
             dup_mem_intrinsic(b, intrin, aligned_offset,
                               requested.align, 0, NULL,
                               requested.num_components, requested.bit_size);
+
+         if (nir_intrinsic_has_base(load))
+            nir_intrinsic_set_base(load, 0);
 
          unsigned max_pad = requested.align - chunk_align;
          unsigned requested_bytes =
@@ -321,6 +326,8 @@ lower_mem_store(nir_builder *b, nir_intrinsic_instr *intrin,
    const uint32_t align_mul = nir_intrinsic_align_mul(intrin);
    const uint32_t whole_align_offset = nir_intrinsic_align_offset(intrin);
    const uint32_t whole_align = nir_intrinsic_align(intrin);
+   const uint32_t base =
+      nir_intrinsic_has_base(intrin) ? nir_intrinsic_base(intrin) : 0;
    const enum gl_access_qualifier access =
       nir_intrinsic_has_access(intrin) ? nir_intrinsic_access(intrin) : 0;
    nir_src *offset_src = nir_get_io_offset_src(intrin);
@@ -401,7 +408,7 @@ lower_mem_store(nir_builder *b, nir_intrinsic_instr *intrin,
          /* TODO add support for offset_shift. */
          assert(!nir_intrinsic_has_offset_shift(intrin) ||
                 nir_intrinsic_offset_shift(intrin) == 0);
-         nir_def *chunk_offset = nir_iadd_imm(b, offset, chunk_start);
+         nir_def *chunk_offset = nir_iadd_imm(b, offset, chunk_start + base);
          nir_def *pad = chunk_align < 4 ? nir_iand_imm(b, chunk_offset, align_mask) : nir_imm_intN_t(b, 0, chunk_offset->bit_size);
          chunk_offset = nir_iand_imm(b, chunk_offset, ~align_mask);
 
@@ -451,12 +458,10 @@ lower_mem_store(nir_builder *b, nir_intrinsic_instr *intrin,
          case nir_intrinsic_store_shared:
             nir_shared_atomic(b, 32, chunk_offset, iand_mask,
                               .atomic_op = nir_atomic_op_iand,
-                              .access = nir_intrinsic_access(intrin),
-                              .base = nir_intrinsic_base(intrin));
+                              .access = nir_intrinsic_access(intrin));
             nir_shared_atomic(b, 32, chunk_offset, data,
                               .atomic_op = nir_atomic_op_ior,
-                              .access = nir_intrinsic_access(intrin),
-                              .base = nir_intrinsic_base(intrin));
+                              .access = nir_intrinsic_access(intrin));
             break;
          case nir_intrinsic_store_scratch: {
             nir_def *value = nir_load_scratch(b, 1, 32, chunk_offset);